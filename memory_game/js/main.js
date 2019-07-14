68705233c3b361899d9543a1d572c318f496004f
mayfielc <christopher.mayfield72@gmail.com>
2019-07-14 18:59:10 -0500

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,30 @@
-console.log("Up and running");
-var cardOne = "queen" ;
-var cardTwo = "queen" ;
-var cardThree = "king" ;
-var cardFour = "king" ;
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+  cardsInPlay.push('cardOne');
+  cardsInPlay.push('cardTwo');
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = [];
+var cardFour = [];
+
+// if(cardsInPlay.length === 2) {
+//   console.log("you have selected two cars");
+// } if(cardsInPlay[0] === cardsInPlay[1]); {
+//   alert("You found a match!");
+// } else {
+//   alert("Sorry, try again");
+// }
+
+if (cardsInPlay.length === 2) {
+   console.log("you have selected two cards");
+if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match!");
+else alert("Sorry, try again"); } 
+
+
+
+
+
+console.log("User flipped queen");
+console.log("User flipped king");
